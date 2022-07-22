68902ecd955b76120cb1687f05baa5ff069207c1
Evan Pratten <ewpratten@gmail.com>
2022-07-22 10:30:56 -0400

    auto db update

--- install.sh
+++ install.sh
@@ -14,3 +14,7 @@ for file in icons/*.png; do
     echo "Linking ICON $file..."
     ln -sf $file $HOME/.icons
 done
+
+# Update the DB
+echo "Updating the .DESKTOP DB..."
+update-desktop-database ~/.local/share/applications
